6f0dde2cd3ffd7ee179965e5a0897f9c3d653bc0
cvaize <cvaize@gmail.com>
2020-07-07 16:53:07 +0500

    (Feat) Footer in process

---
+++ components/Btn.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="tag" class="btn" @click="$emit('click', $event)">
+    <b><slot></slot></b>
+  </component>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Btn',
+  props: {
+    tag: {
+      type: String,
+      default: () => 'button',
+      required: false,
+    },
+  },
+})
+</script>
+
+<style lang="scss"></style>
--- components/Footer.vue
+++ components/Footer.vue
@@ -7,21 +7,120 @@
         &copy; 2020 Gravity Protocol. Oracles and Cross-chain Communication
         Network
       </div>
-      <div class="footer-cookies">
-        We use cookies on our website. By continuing to use the site, or by
-        clicking “I agree”, you consent to the use of cookies. For more info
-        click here.
+    </div>
+    <div v-if="isCookiesBox" class="footer-cookies" :style="getCookiesStyle">
+      <div class="footer-cookies-wrapper">
+        <div class="container">
+          <div ref="cookiesBox" class="footer-cookies-box">
+            <div>
+              We use cookies on our website. By continuing to use the site, or
+              by clicking “I agree”, you consent to the use of cookies. For more
+              info click here.
+            </div>
+            <btn
+              class="btn-outline-primary footer-cookies-btn"
+              @click="hideCookiesBox"
+              >I agree</btn
+            >
+          </div>
+        </div>
       </div>
     </div>
   </footer>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Vue from 'vue'
+import Btn from '~/components/Btn.vue'
+
+export default Vue.extend({
+  components: { Btn },
+  data: () => ({
+    isCookiesBox: true,
+    heightCookiesBox: 100,
+    instanceHeightCookiesBox: undefined,
+  }),
+  computed: {
+    getCookiesStyle() {
+      return {
+        height: this.heightCookiesBox + 30 + 'px',
+      }
+    },
+  },
+  mounted() {
+    this.calcHeightCookiesBox()
+    this.bindHeightCookiesBox()
+  },
+  beforeDestroy() {
+    this.unbindHeightCookiesBox()
+  },
+  methods: {
+    hideCookiesBox() {
+      this.isCookiesBox = false
+      this.unbindHeightCookiesBox()
+    },
+    bindHeightCookiesBox() {
+      if (this.isCookiesBox && typeof window !== 'undefined') {
+        window.addEventListener('resize', this.calcHeightCookiesBox)
+      }
+    },
+    unbindHeightCookiesBox() {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', this.calcHeightCookiesBox)
+      }
+    },
+    calcHeightCookiesBox() {
+      if (this.isCookiesBox) {
+        clearTimeout(this.instanceHeightCookiesBox)
+        // @ts-ignore: Type 'Timeout' is not assignable to type 'undefined'.
+        this.instanceHeightCookiesBox = setTimeout(() => {
+          if (this.$refs.cookiesBox) {
+            // @ts-ignore:  Property 'getBoundingClientRect' does not exist on type 'Element | Element[] | Vue | Vue[]'
+            this.heightCookiesBox = this.$refs.cookiesBox.getBoundingClientRect().height
+          }
+        }, 1000)
+      }
+    },
+  },
+})
 </script>
 
 <style lang="scss">
+.footer-cookies {
+  min-height: 100px;
+}
+.footer-cookies-wrapper {
+  position: fixed;
+  bottom: 30px;
+  left: 0;
+  width: 100%;
+}
+.footer-cookies-box {
+  background: $body-bg;
+  border: 1px solid #2e3148;
+  min-height: 70px;
+  padding: 15px;
+  @include media-breakpoint-up(sm) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+.footer-nav {
+  height: 118px;
+}
+.footer-socials {
+  padding-top: 47px;
+  padding-bottom: 43px;
+}
 .footer-copyright {
-  margin-bottom: 38px;
+  padding-bottom: 38px;
+}
+.footer-cookies-btn {
+  margin-top: 19px;
+  @include media-breakpoint-up(sm) {
+    margin-top: 0;
+    margin-left: 33px;
+  }
 }
 </style>
