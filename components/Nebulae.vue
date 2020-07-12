46b362fee4f1e8f813e2f64e071418a3da15342e
cvaize <cvaize@gmail.com>
2020-07-12 21:13:43 +0500

    (Feat) Auth

--- components/Nebulae.vue
+++ components/Nebulae.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
     <tabs-and-search></tabs-and-search>
-    <table-block>
+    <table-block style="margin-bottom: 34px;">
       <template v-slot:head>
         <tr>
-          <th style="width: 10%;">Name</th>
-          <th>Status</th>
+          <th>Name</th>
+          <th style="width: 100px;">Status</th>
           <th>Data Feed</th>
           <th>Description</th>
-          <th>Threshold Score<br />(0-10)</th>
+          <th style="width: 140px;">Threshold Score<br />(0-10)</th>
           <th>Target chain</th>
           <th>Regularity<br />(hour/day)</th>
           <th>Subscription fee</th>
@@ -16,19 +16,24 @@
       </template>
       <template v-slot:body>
         <tr v-for="index in 20" :key="index">
-          <td style="width: 10%;">Nebula @Demo</td>
-          <td>
+          <td>Nebula @Demo</td>
+          <td style="width: 100px;">
             Active
           </td>
-          <td>Demo</td>
+          <td>Binance-Huobi-BTC_USD</td>
           <td>Demo Nebula to accumulate Gravity score.</td>
-          <td>0.1</td>
+          <td style="width: 140px;">0.1</td>
           <td>Waves, ID=1</td>
           <td>~per hour</td>
           <td>~10 WAVES | $12.6</td>
         </tr>
       </template>
     </table-block>
+    <div class="text-right" style="margin-bottom: 34px;">
+      <btn class="btn-primary" style="padding-left: 70px; padding-right: 70px;"
+        >Add Nebula</btn
+      >
+    </div>
   </div>
 </template>
 
@@ -36,12 +41,14 @@
 import Vue from 'vue'
 import TableBlock from '~/components/Table.vue'
 import TabsAndSearch from '~/components/TabsAndSearch.vue'
+import Btn from '~/components/Btn.vue'
 
 export default Vue.extend({
   name: 'Nebulae',
   components: {
     TableBlock,
     TabsAndSearch,
+    Btn,
   },
 })
 </script>
