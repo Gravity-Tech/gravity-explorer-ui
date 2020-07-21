628bc8dbb905b75ae69fbf560547f59f766982cf
Shamil Khalilov <shamfama@gmail.com>
2020-07-21 16:44:41 +0300

    Add GTAG

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,9 +29,23 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
       { rel: 'manifest', href: '/favicon/site.webmanifest' },
       {
         rel: 'stylesheet',
@@ -66,7 +80,15 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: ['@nuxt/typescript-build'],
+  buildModules: [
+    '@nuxt/typescript-build',
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: 'UA-168552658-3',
+      },
+    ],
+  ],
   /*
    ** Nuxt.js modules
    */
